10bd4b27fc19fdb83f9e9e16f15906677e446425
smart-techno <mickael.kasinski@gmail.com>
2023-02-13 12:58:08 +0100

    Gpt for Qwant update

--- src/content-script/ChatGPTContainer.tsx
+++ src/content-script/ChatGPTContainer.tsx
@@ -1,10 +1,5 @@
-import { useState } from 'react'
-import useSWRImmutable from 'swr/immutable'
-import { fetchPromotion } from '../api'
 import { TriggerMode } from '../config'
 import ChatGPTCard from './ChatGPTCard'
-import { QueryStatus } from './ChatGPTQuery'
-import Promotion from './Promotion'
 
 interface Props {
   question: string
@@ -12,23 +7,10 @@ interface Props {
 }
 
 function ChatGPTContainer(props: Props) {
-  const [queryStatus, setQueryStatus] = useState<QueryStatus>()
-  const query = useSWRImmutable(
-    queryStatus === 'success' ? 'promotion' : undefined,
-    fetchPromotion,
-    { shouldRetryOnError: false },
-  )
   return (
-    <>
-      <div className="chat-gpt-card">
-        <ChatGPTCard
-          question={props.question}
-          triggerMode={props.triggerMode}
-          onStatusChange={setQueryStatus}
-        />
-      </div>
-      {query.data && <Promotion data={query.data} />}
-    </>
+    <div className="chat-gpt-card">
+      <ChatGPTCard question={props.question} triggerMode={props.triggerMode} />
+    </div>
   )
 }
 
